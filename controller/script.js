f2bdb3750f59221bc61e3b1b90ee7fac51691263
Laurel Lao Ang <laurellaoang@gmail.com>
2022-10-10 10:51:56 +0800

    add scripts

---
+++ controller/script.js
@@ -0,0 +1,26 @@
+$("#button").click(function () {
+    fetch_Users();
+    });
+    
+constcreate_Li = (user) => {
+const li = document.createElement("li");
+li.textContent = `${user.id}: ${user.first_name} ${user.last_name}`;
+return li;
+};
+
+constappendToDOM = (users) => {
+const ul = document.querySelector("ul");
+users.map((user) => {
+ul.appendChild(create_Li(user));
+});
+};
+
+constfetch_Users = () => {
+axios
+.get("https://reqres.in/api/users")
+.then((response) => {
+const users = response.data.data;
+appendToDOM(users);
+})
+.catch((error) =>console.error(error));
+};
