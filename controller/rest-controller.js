81700a0ee989d18b2e1106cd4cecb65f3f3326b3
Laurel Lao Ang <laurellaoang@gmail.com>
2022-10-10 10:44:09 +0800

    add button

--- controller/rest-controller.js
+++ controller/rest-controller.js
@@ -5,3 +5,22 @@ const fetch_Users = () => {
         })
         .catch((error) =>console.error(error));
     };
+
+const appendToDOM = (users) => {
+    const ul = document.querySelector("ul");
+    users.map((user) => {
+    ul.appendChild(create_Li(user));
+    });
+    };
+
+constcreate_Li = (user) => {
+    const li = document.createElement("li");
+    li.textContent = `${user.id}: ${user.first_name} ${user.last_name}`;
+    return li;
+    };
+
+$("#button").click(function () {
+
+    fetch_Users();
+    
+    });
